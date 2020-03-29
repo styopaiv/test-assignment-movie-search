0fbd70cfd4d1cba2ca216df65d3b70a90677ea35
Stepan Ivanov <s.ivanov@unitedtraders.com>
2020-03-30 00:19:30 +0300

    Updates

---
+++ precache-manifest.0d3c1e65331afc985da07552f5a434cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0cde9b88917522c2c3f1a8adb92ee26",
+    "url": "/test-assignment-brandslisten/index.html"
+  },
+  {
+    "revision": "37295a650c7f19c0072c",
+    "url": "/test-assignment-brandslisten/static/js/2.a62879cd.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/test-assignment-brandslisten/static/js/2.a62879cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b50336c1cd04f8ab618",
+    "url": "/test-assignment-brandslisten/static/js/main.eb55e22f.chunk.js"
+  },
+  {
+    "revision": "7942bfb88d1e0d01f613",
+    "url": "/test-assignment-brandslisten/static/js/runtime-main.11d07fcd.js"
+  }
+]);
